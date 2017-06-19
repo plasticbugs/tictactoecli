7ce52abb5ed60f4c6bce4daf4a21a5afb1109fb4
Scott Moschella <scottmosch@gmail.com>
2017-06-19 15:12:26 -0700

    Create a printBoard function that shows the current state of the board

--- tictactoe.js
+++ tictactoe.js
@@ -1,9 +1,28 @@
 var prompt = require('prompt');
-
+console.log(`Player 1 is Xs, Player 2 is Os`);
 prompt.start();
 
-console.log("Player 1, your turn...")
+var TicTacToe = function() {
+  let gameBoard = [[" ", "O", "X"],[" ", "O", " "],["X", " ", " "]];
 
-prompt.get(['turn'], (err, result) => {
-  console.log("you put: ", result.turn)
-})
+  const printGameboard = (board) => {
+    console.log(`|${board[0][0]}|${board[0][1]}|${board[0][2]}|`);
+    console.log(`-------`);
+    console.log(`|${board[1][0]}|${board[1][1]}|${board[1][2]}|`);
+    console.log(`-------`);
+    console.log(`|${board[2][0]}|${board[2][1]}|${board[2][2]}|`);
+  }
+  printGameboard(gameBoard);
+}
+
+console.log(`Player, what row?`);
+console.log(`Player, what column?`);
+
+
+TicTacToe();
+
+
+
+// prompt.get([{name: 'turn', description: 'Enter your name'}, {name: 'password', description: 'Enter your password'}], (err, result) => {
+//   console.log("you put: ", result.username, result.password)
+// })
